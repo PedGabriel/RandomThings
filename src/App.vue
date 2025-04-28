867046e8fcf7c91e51fd8d17d9bf6a6d971f1f12
PedGabriel <pedrogabrielgoncalves91@gmail.com>
2025-04-28 18:04:24 -0300

    FEAT: Mudança na ideia central do site finalizada #3

--- src/App.vue
+++ src/App.vue
@@ -7,36 +7,35 @@ const produtoDestaque = ref({})
 const linkFakeStore = 'https://fakestoreapi.com/products'
 
 async function buscarProdutos() {
-try {
-  const respostaStore = await fetch(linkFakeStore)
-  const informacoes = await respostaStore.json()
+  try {
+    const respostaStore = await fetch(linkFakeStore)
+    const informacoes = await respostaStore.json()
 
-  const aleatorio = informacoes[Math.floor(Math.random() * informacoes.length)];
-  produtoDestaque.value = aleatorio;
+    const aleatorio = informacoes[Math.floor(Math.random() * informacoes.length)]
+    produtoDestaque.value = aleatorio
 
-  produtos.value = informacoes.map((produto) => ({
-    id: produto.id,
-    titulo: produto.title,
-    desc: produto.description,
-    preco: produto.price,
-    imagem: produto.image,
-    link: '#',
-  }))
+    produtos.value = informacoes.map((produto) => ({
+      id: produto.id,
+      titulo: produto.title,
+      desc: produto.description,
+      preco: `$` + produto.price,
+      imagem: produto.image,
+      link: '#',
+      favoritado: false,
+      comprado: false,
+    }))
   } catch (error) {
-    console.error('Erro ao buscar produtos:', error);
+    console.error('Erro ao buscar produtos:', error)
   }
 }
 onMounted(() => {
   buscarProdutos()
 })
-
 </script>
 
 <template>
   <header>
-    <div class="logo">
-      <p>Random Things</p>
-    </div>
+      <h1>RandomThings</h1>
     <div class="barraDePesquisa">
       <input type="text" placeholder="Pesquisar" />
     </div>
@@ -82,7 +81,19 @@ onMounted(() => {
     <section class="produtos">
       <ul v-for="(produto, index) in produtos" :key="index">
         <li>
-          {{ produto }}
+          <img :src="produto.imagem" alt="Imagem de produto" />
+          <h3>{{ produto.titulo }}</h3>
+          <p>{{ produto.preco }}</p>
+          <div class="botaoOutrasAbas">
+            <button class="botaoComprar" v-if="produto.comprado == false" @click="produto.comprado = true">COMPRAR AGORA</button>
+            <button class="botaoComprado" v-else @click="produto.comprado = false">COMPRADO</button>
+            <span
+              @click="produto.favoritado = true"
+              v-if="produto.favoritado == false"
+              class="fa-regular fa-heart"
+            ></span>
+            <span @click="produto.favoritado = false" v-else class="fa-solid fa-heart"></span>
+          </div>
         </li>
       </ul>
     </section>
@@ -90,9 +101,8 @@ onMounted(() => {
 </template>
 
 <style scoped>
-
 /*Geral*/
-header{
+header {
   margin: 2vw 10vw 3vw 10vw;
 }
 section {
@@ -102,10 +112,10 @@ footer {
   margin: 0 5vw 2vw 5vw;
 }
 hr {
-  color: #F85525;
+  color: #f85525;
   text-decoration: none;
   border: solid;
-  border-width: 0.22vw;
+  border-width: 0.34vw;
   margin: 1vw 0 1vw 0;
   margin: 1vw 0 1vw 0;
 }
@@ -113,19 +123,25 @@ hr {
 /*CABEÇALHO*/
 header {
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   font-size: 1.5rem;
   font-weight: 500;
 }
-div.logo {
-  display: flex;
+h1{
+  margin-right: 4vw;
+  font-weight: 600;
+  font-size: 1.7rem;
 }
 div.barraDePesquisa input {
   font-size: 1.2rem;
+  background-color: white;
+  border: 3px solid #121619;
 }
-div.abas ul{
+div.abas ul {
   display: flex;
   justify-content: space-between;
+  margin-right: 2vw;
+  margin-left: 2vw;
 }
 div.abas ul li {
   margin-right: 1vw;
@@ -133,11 +149,11 @@ div.abas ul li {
 }
 div.cards {
   display: flex;
-  color: #F85525;
+  color: #f85525;
 }
 div.cards span {
-margin: 0 0.5vw 0 0.5vw;
-font-size: 1.8rem;
+  margin: 0 0.5vw 0 0.5vw;
+  font-size: 1.8rem;
 }
 
 /*item em destaque*/
@@ -145,9 +161,24 @@ font-size: 1.8rem;
 section.destaque div.infoDoDestaque {
   display: flex;
   justify-content: space-between;
+  margin-top: 2vw;
+  margin-bottom: 2vw;
+  align-items: center;
+  gap: 3vw;
 }
 section.destaque div.textosDest {
   width: 60%;
+}
+section.destaque div.imagemDestaque {
+  width: 40%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+section.destaque img{
+  max-width: 100%;
+  max-height: 400px;
+  border: 3px solid #f85525;
 }
 section.destaque h2 {
   font-size: 2rem;
@@ -160,12 +191,73 @@ section.destaque p {
 }
 section.destaque button {
   margin-top: 2vw;
-  background-color: #F85525;
+  background-color: #f85525;
   border-color: #121619;
   padding: 0.5vw 1vw 0.5vw 1vw;
   font-size: 1.2rem;
 }
 section.destaque button:hover {
   background-color: #c74721;
+}
+
+/*Produtos*/
+
+section.produtos {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+section.produtos li {
+  margin: 1vw;
+  width: 240px;
+  height: 340px;
+  border: solid 3px #f85525;
+  user-select: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+section.produtos li:hover {
+  transform: scale(1.1);
+  transition: 0.2s;
+}
+section.produtos img {
+  object-fit: cover;
+  width: 80%;
+  height: 170px;
+  margin-top: 0.5vw;
+  box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.178);
+}
+section.produtos h3 {
+  margin: 1vw;
+  font-weight: 500;
+}
+section.produtos p {
+  margin: 0 1vw 1vw 1vw;
+}
+section.produtos div button {
+  margin-right: 1vw;
+  border-color: #121619;
+  border: 1.5px solid;
+  font-weight: 500;
+}
+section.produtos div button.botaoComprar {
+  background-color: #f85525;
+}
+section.produtos div button.botaoComprado {
+  background-color: rgb(204, 204, 204);
+  color: #485055;
+}
+section.produtos div button:hover {
+  transition: 0.2s;
+  transform: scale(1.1);
+}
+section.produtos div span {
+  margin-left: 1vw;
+  color: rgb(255, 36, 36);
+}
+section.produtos div span:hover{
+  transform: scale(1.2);
+  transition: 0.2s;
 }
 </style>
